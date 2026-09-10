a1ae00f488af2f5a8ffdd91ef00f2ffd5769d2c4
Gonzalez230 <benjagonzalez230@gmail.com>
2026-09-10 10:40:00 -0300

    2.1.8: se agrego el ambito del carrito

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -20,12 +20,15 @@ export const environment = {
   /** Configuración de Azure Entra ID (OAuth2) para producción */
   azure: {
     clientId: '719c999d-0f57-4ad5-9bd9-a72be5ca07e0',
-    
+
     authority: 'https://login.microsoftonline.com/e5372bf0-c5e3-4286-887c-79069f209c1f',
-    
+
     // Debe coincidir con el origen HTTPS certificado y registrado en Entra ID.
     redirectUri: 'https://100.49.172.129/',
-    
-    scopes: ['api://719c999d-0f57-4ad5-9bd9-a72be5ca07e0/Autenticacion.Microsoft'],
+
+    scopes: [
+      'api://719c999d-0f57-4ad5-9bd9-a72be5ca07e0/Autenticacion.Microsoft',
+      'api://719c999d-0f57-4ad5-9bd9-a72be5ca07e0/Cart.Write'
+    ]
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -20,10 +20,13 @@ export const environment = {
   /** Configuración de Azure Entra ID (OAuth2) */
   azure: {
     clientId: '719c999d-0f57-4ad5-9bd9-a72be5ca07e0',
-    
+
     authority: 'https://login.microsoftonline.com/e5372bf0-c5e3-4286-887c-79069f209c1f',
     redirectUri: 'https://100.49.172.129/',
-    
-    scopes: ['api://719c999d-0f57-4ad5-9bd9-a72be5ca07e0/Autenticacion.Microsoft'],
+
+    scopes: [
+      'api://719c999d-0f57-4ad5-9bd9-a72be5ca07e0/Autenticacion.Microsoft',
+      'api://719c999d-0f57-4ad5-9bd9-a72be5ca07e0/Cart.Write'
+    ],
   },
 };
